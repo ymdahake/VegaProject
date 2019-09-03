5484c78385f08ec869dcaee1f3a4968e10f7eb14
Yogesh Dahake <ymdahake@gmail.com>
2019-09-03 23:42:05 +0530

    Added Models menu Category Item restaurant

---
+++ Vega/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vega.Models
+{
+    public class MenuItem
+    {
+        public int MenuIteamId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        
+        public virtual MenuCategory MenuCategory { get; set; }
+    }
+}
---
+++ Vega/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vega.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public String Status { get; set; }
+
+        public int PerPersonAmount { get; set; }
+
+        public virtual List<MenuCategory> MenuCategories { get; set; }
+
+
+    }
+}
